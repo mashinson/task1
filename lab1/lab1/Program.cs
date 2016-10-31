e18d380463f300e4ca89a445ded1e968edef5d44
mashinson <mashinson1@gmail.com>
2016-11-01 00:23:49 +0200

    lab1
    max and min from 5

---
+++ lab1/lab1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1 = 0,
+                x2 = 0,
+                x3 = 0,
+                x4 = 0,
+                x5 = 0;
+            Console.WriteLine("Введите 5 чисел: ");
+            x1 = Convert.ToDouble(Console.ReadLine());
+            x2 = Convert.ToDouble(Console.ReadLine());
+            x3 = Convert.ToDouble(Console.ReadLine());
+            x4 = Convert.ToDouble(Console.ReadLine());
+            x5 = Convert.ToDouble(Console.ReadLine());
+
+            double max = x1,
+                min = x1;
+
+            // max
+            if (x2 > max) max = x2;
+            if (x3 > max) max = x3;
+            if (x4 > max) max = x4;
+            if (x5 > max) max = x5;
+            Console.WriteLine("Max из пяти чисел: {0} ", max);
+
+            // min
+            if (x2 < min) min = x2;
+            if (x3 < min) min = x3;
+            if (x4 < min) min = x4;
+            if (x5 < min) min = x5;
+            Console.WriteLine("Min из пяти чисел: {0} ", min);
+
+            Console.ReadLine();
+        }
+    }
+}
